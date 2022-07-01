5393e26551a5a47b0711d94029c478b96300dc33
Ubuntu <ubuntu@ip-172-31-10-35.eu-west-1.compute.internal>
2022-07-01 08:16:17 +0000

    dia 5

--- despliegues/script_despliegue_servidor_aws/vars.tf
+++ despliegues/script_despliegue_servidor_aws/vars.tf
@@ -1,20 +1,3 @@
-
-variable "regionAWS" {
-
-    type = string 
-    
-    description = "Region de amazon donde desplegar"
-    
-    validation {
-        condition     = can(regex("^[a-z]{2}-[a-z]+-[1-9]$", var.regionAWS))
-        error_message = "La región suministrada no es válida."
-    }
-    
-    nullable = false
-    
-    default  = "eu-west-1"
-}
-
 variable "ficherosClave" {
 
     type = object({
@@ -23,32 +6,9 @@ variable "ficherosClave" {
     })
     
     description = "Ficheros donde guardar las claves"
-    
-    validation  {
-        condition = length(
-                        regexall("^[/]?([A-Za-z0-9_.-]+[/]?)*[.]pem$",
-                                 var.ficherosClave.privada )
-                    ) == 1 
-        error_message = "La ruta del host para la clave privada no es válida"
-    }
-    
-    validation  {
-        condition = length(
-                        regexall("^[/]?([A-Za-z0-9_.-]+[/]?)*[.]pem$",
-                                 var.ficherosClave.publica )
-                    ) == 1 
-        error_message = "La ruta del host para la clave publica no es válida"
-    }
-    
-    nullable = false
-    
-    default  = {
-        privada = "clave-privada.pem"
-        publica = "clave-publica.pem"
-    }
-    
-}
 
+    nullable = false
+}
 
 # Borrar claves on destroy
 
@@ -56,31 +16,4 @@ variable "borrarFicherosDeClavesAlDestruir" {
     type = bool
     description = "Borrar los ficheros de claves ssh al destruir la infraestructura"
     nullable = false
-    default = false
-}
-
-variable "nombreDespliegue" {
-    type = string
-    description = "Nombre que se añade a los recursos de amazon"
-    nullable = false
-    default = false
-}
-variable "tipoInstancia" {
-    type = string
-    description = "Tipo de instancia a crear"
-    nullable = false
-    default =  "t2.micro"
-    
-    validation {
-        condition     = can(regex("^[a-z0-9]{2,}[.][a-z]+$", var.tipoInstancia))
-        error_message = "El tipo de instancia no es válido."
-    }
-}
-
-
-variable "probarConexion" {
-    type = bool
-    description = "Hace una prueba de conexion con la maquina"
-    nullable = false
-    default = true
 }
